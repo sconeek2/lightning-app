ac2e1e20c0b97c28896f5c0b0c58ed4b86b17b9f
sconeek2 <neelsk2@gmail.com>
2023-08-24 14:39:04 -0700

    display yyyy/mm/dd to mm/dd/yyy

--- src/Components/Schedule.js
+++ src/Components/Schedule.js
@@ -1,16 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import game_schedule from "../Data/schedule-game.json";
 
 const Schedule = () => {
-  
   // custom function to sory the json by date
   function custom_sort(a, b) {
-    return (
-      new Date(a.Date).getTime() - new Date(b.Date).getTime()
-    );
+    return new Date(a.Date).getTime() - new Date(b.Date).getTime();
   }
 
   game_schedule.sort(custom_sort);
+
+  const formatDate = (inputDate) => {
+    const dateParts = inputDate.split("/");
+    const year = parseInt(dateParts[0]);
+    const month = parseInt(dateParts[1]);
+    const day = parseInt(dateParts[2]);
+
+    const formattedDate = new Date(year, month - 1, day);
+
+    const options = { month: "2-digit", day: "2-digit", year: "numeric" };
+    return formattedDate.toLocaleDateString(undefined, options);
+  };
 
   return (
     <div className="rosterContainer">
@@ -26,7 +35,7 @@ const Schedule = () => {
         <tbody>
           {game_schedule.map((item) => (
             <tr key={item.Date}>
-              <td>{item.Date}</td>
+              <td>{formatDate(item.Date)}</td>
               <td>{item.Time}</td>
               <td>{item.City}</td>
               <td>{item.State}</td>
