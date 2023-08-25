de840ad560e2964903e4653ac4b280467c45a78b
sconeek2 <neelsk2@gmail.com>
2023-08-25 13:52:01 -0700

    moved date function to standalone file

--- src/Components/Schedule.js
+++ src/Components/Schedule.js
@@ -1,5 +1,6 @@
 import React from "react";
 import game_schedule from "../Data/schedule-game.json";
+import { formatDate } from "./dateUtils";
 
 const Schedule = () => {
   // custom function to sory the json by date
@@ -8,18 +9,6 @@ const Schedule = () => {
   }
 
   game_schedule.sort(custom_sort);
-
-  const formatDate = (inputDate) => {
-    const dateParts = inputDate.split("/");
-    const year = parseInt(dateParts[0]);
-    const month = parseInt(dateParts[1]);
-    const day = parseInt(dateParts[2]);
-
-    const formattedDate = new Date(year, month - 1, day);
-
-    const options = { month: "2-digit", day: "2-digit", year: "numeric" };
-    return formattedDate.toLocaleDateString(undefined, options);
-  };
 
   return (
     <div className="rosterContainer">
