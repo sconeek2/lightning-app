ae8b33223746889b9eb37b0d92f556714ef43b82
sconeek2 <31807708+sconeek2@users.noreply.github.com>
2023-09-01 09:18:38 -0700

    Update Hero.js

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,67 +1,18 @@
 import React from "react";
-import {
-  MDBNavbar,
-  MDBNavbarToggler,
-  MDBIcon,
-  MDBNavbarNav,
-  MDBNavbarItem,
-  MDBNavbarLink,
-  MDBContainer,
-} from "mdb-react-ui-kit";
 import "../CSS/Hero.css";
-import team from "../Images/net.png";
+import backgroundVid from '../Images/video.mp4';
+
+
 
 export default function BgImageWithNavbar() {
   return (
-    <header >
-      {/* <MDBNavbar expand="lg" light bgColor="white" id='navbar-container'>
-        <MDBContainer fluid id='test'>
-          <MDBNavbarToggler
-            aria-controls="navbarExample01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <MDBIcon fas icon="bars" />
-          </MDBNavbarToggler>
-          <div className="collapse navbar-collapse" id="navbarExample01">
-            <MDBNavbarNav right className="mb-2 mb-lg-0">
-              <MDBNavbarItem active>
-                <MDBNavbarLink aria-current="page" href="/Home">
-                  Home
-                </MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href="/Roster">Roster</MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href="/Schedule">Schedule</MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href="SnackForm">Snacks</MDBNavbarLink>
-              </MDBNavbarItem>
-              <MDBNavbarItem>
-                <MDBNavbarLink href="/Stats">Stats</MDBNavbarLink>
-              </MDBNavbarItem>
-            </MDBNavbarNav>
-          </div>
-        </MDBContainer>
-      </MDBNavbar> */}
+    <div className='video_container'>
 
-      <div
-        className="p-5 text-center bg-image"
-        id="background"
-        style={{
-          backgroundImage: `url(${team})`,
-        }}
-      >
-        <div className="mask">
-          <div className="d-flex justify-content-center align-items-center h-100">
-            <div className="text-white">
-              <h1 className="mb-3">TSC Lightning</h1>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
+      <video autoPlay loop muted id='video'>
+        <source src={backgroundVid} type='video/mp4' />
+      </video>
+      <h1>TSC Lightning</h1>
+
+    </div>
   );
 }
