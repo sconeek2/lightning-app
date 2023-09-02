3c1b37774e48cd182e38d1b04376637d470ec536
sconeek2 <neelsk2@gmail.com>
2023-09-01 17:02:12 -0700

    reworked the stats page

---
+++ src/Components/CreateTable.js
@@ -0,0 +1,31 @@
+import React from "react";
+import game_data from "../Data/combined_game_data.json";
+
+const createTable = (props) => {
+
+  return (
+    <div>
+      <h2>Game {props.num}</h2>
+      <table id={`game${props.num}`} className="game-stats">
+        <thead>
+          <tr>
+            <th>Player</th>
+            <th>Minutes</th>
+            <th>Goals</th>
+          </tr>
+        </thead>
+        <tbody>
+          {game_data[`game${props.num}`].map((item) => (
+            <tr>
+              <td>{item.Name}</td>
+              <td>{item.Play_Time}</td>
+              <td>{item.Goal}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default createTable;
--- src/Components/Stats.js
+++ src/Components/Stats.js
@@ -1,38 +1,16 @@
 import React from "react";
-import player_stats from "../Data/player_stats.json";
+import CreateTable from "./CreateTable";
 
 const Stats = () => {
   return (
     <div className="rosterContainer">
-      <table id="roster">
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Position</th>
-            <th>Goals</th>
-            <th>Assists</th>
-            <th>Yellow Cards</th>
-            <th>Red Cards</th>
-            <th>MPG</th>
-          </tr>
-        </thead>
-        <tbody>
-          {player_stats.map((item) => (
-            <tr key={item.player_id}>
-              <td>{item.first_name}</td>
-              <td>{item.last_name}</td>
-              <td>{item.position}</td>
-              <td>{item.goals_scored}</td>
-              <td>{item.assists}</td>
-              <td>{item.yellow_cards}</td>
-              <td>{item.red_cards}</td>
-              <td>{item.minutes_played}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+      <CreateTable num={1} />
+      <CreateTable num={2} />
+      <CreateTable num={3} />
+      <CreateTable num={4} />
+      <CreateTable num={5} />
+      <CreateTable num={6} />
+    </div >
   );
 };
 
